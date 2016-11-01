02de3f157fd5b02a24cde502a7894175eb919bbe
Sharon Ye <357275255@qq.com>
2016-11-01 15:26:18 +0800

    add comments

---
+++ js/duoshuo.js
@@ -0,0 +1,11 @@
+//多说公共JS代码
+
+var duoshuoQuery = {short_name:"sharonye"};
+(function() {
+	var ds = document.createElement('script');
+	ds.type = 'text/javascript';ds.async = true;
+	ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+	ds.charset = 'UTF-8';
+	(document.getElementsByTagName('head')[0] 
+	 || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
